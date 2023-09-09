417ceefa9f3e3cf75df81c255bca60519cd81208
bkkom <seoy1108@gmail.com>
2023-09-09 15:55:34 +0900

    Chore: config 파일 코드 작성

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ["babel-loader", "ts-loader"],
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    hot: true,
+    open: true,
+  },
+};
