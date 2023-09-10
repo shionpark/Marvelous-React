6f148ed4adefacd310dfde48e07be83930f25883
bkkom <seoy1108@gmail.com>
2023-09-10 14:48:37 +0900

    Chore: index.html 및 파일 생성

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1>REACT APP</h1>
+    </>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,14 @@
-let test: string = "Hello world";
+import React from "react";
+import CreateDOM from "react-dom/client";
+
+import App from "./App";
+
+const root = document.getElementById("root");
+if (!root) throw new Error("Failed to find the root element");
+const appRoot = CreateDOM.createRoot(root);
+
+appRoot.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
