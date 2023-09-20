de52d8eb5d42d2d447cf675a53e11d466739ef5c
bkkom <seoy1108@gmail.com>
2023-09-20 23:59:27 +0900

    Feat: Error 페이지 추가

--- src/routes/RootRouter.tsx
+++ src/routes/RootRouter.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 import App from "@/App";
-import { Home, Detail } from "../pages";
+import { Home, Detail, ErrorPage } from "../pages";
 
 const RootRouter = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    errorElement: <Home />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
